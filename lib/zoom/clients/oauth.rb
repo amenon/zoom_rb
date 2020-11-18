d401c6667e857b52462228f31c45dbb6bc4e9a9b
Arvind Menon <amenon81@gmail.com>
2020-11-17 17:21:33 -0800

    Allow auth_token requests

--- lib/zoom/clients/oauth.rb
+++ lib/zoom/clients/oauth.rb
@@ -14,7 +14,7 @@ module Zoom
       #
       def initialize(config)
         Zoom::Params.new(config).permit( %i[auth_token auth_code redirect_uri access_token refresh_token timeout])
-        Zoom::Params.new(config).require_one_of(%i[access_token refresh_token auth_code])
+        Zoom::Params.new(config).require_one_of(%i[auth_token access_token refresh_token auth_code])
         if (config.keys & [:auth_code, :redirect_uri]).any?
           Zoom::Params.new(config).require( %i[auth_code redirect_uri])
         end
